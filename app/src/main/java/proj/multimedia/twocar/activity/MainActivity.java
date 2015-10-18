f3e10ba27f3099260cd0580dc8c801c3ca59b867
menghanchen <meganmhchen@gmail.com>
2015-10-18 14:08:34 -0400

    1

--- app/src/main/java/proj/multimedia/twocar/activity/MainActivity.java
+++ app/src/main/java/proj/multimedia/twocar/activity/MainActivity.java
@@ -1,18 +1,62 @@
 package proj.multimedia.twocar.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import proj.multimedia.twocar.R;
 
 public class MainActivity extends BaseActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupMessageButton();
+        setupNavigationButton();
+    }
+
+    private void setupMessageButton() {
+        //Switching Activities
+        ImageButton startButton = (ImageButton) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Start!", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+        ImageButton settingButton = (ImageButton) findViewById(R.id.settingButton);
+        settingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SettingActivity.class));
+            }
+        });
+        ImageButton highScoreButton = (ImageButton) findViewById(R.id.highScoreButton);
+        highScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HighScoreActivity.class));
+            }
+        });
+
+    }
+
+    private void setupNavigationButton() {
+        ImageButton quitButton = (ImageButton) findViewById(R.id.quitButton);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
